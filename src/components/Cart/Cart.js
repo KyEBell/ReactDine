12a62f1e8aef998669a03c30ca48d9b8dae1d9bd
KyEBell <kyleevanbell@gmail.com>
2023-10-19 17:06:02 -0400

    form added for customer to enter details on their order

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,6 +3,7 @@ import classes from './Cart.module.css';
 import Modal from '../UI/Modal';
 import CartContext from '../../store/cart-context';
 import CartItem from './CartItem';
+import Checkout from './Checkout';
 
 const Cart = (props) => {
   const cartCntx = useContext(CartContext);
@@ -39,6 +40,7 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
+      <Checkout />
       <div className={classes.actions}>
         <button className={classes['button--alt']} onClick={props.onHideCart}>
           Close
